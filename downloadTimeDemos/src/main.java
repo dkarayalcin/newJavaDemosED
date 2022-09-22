60b22eaeee27c722edc60925897fcc5fd3094f0d
Devrim <72310045+dkarayalcin@users.noreply.github.com>
2022-09-22 18:43:30 +0300

    Update main.java

--- downloadTimeDemos/src/main.java
+++ downloadTimeDemos/src/main.java
@@ -14,16 +14,21 @@ public class main {
         System.out.println("Dosya Boyutunu küsüratsız bir şeklde yazınız. Örn: 125");
 
         double fileSize = sc.nextInt();
+         System.out.println("||==============>");
 
-        // we convert size from Gb to megabyte
-        double fileSizeMb = fileSize * 1024;
+     
+        // we convert Gb in to Mbps format and print out how many minutes we need to download. eta.
+        double fileSizeMb = fileSize * 1024*8 ;
 
-        double dwnlPerSec = fileSizeMb /8;
 
-        double userSpeed = mbpsSpeed/8;
 
-        double avarageTimeSec = dwnlPerSec/userSpeed;
-        double avarageTimeMin = avarageTimeSec ;
+
+        double xxx= fileSizeMb/mbpsSpeed;
+
+
+
+
+        System.out.println("İndirme süreniz ortalama "+ xxx/60 +" dakikadır.");
 
 
         System.out.println("İndirme süreniz ortalama "+ avarageTimeMin +" dakikadır.");
